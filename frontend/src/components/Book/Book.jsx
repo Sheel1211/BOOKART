922fd3ee1bfbac7545acea2d99ad6d51017de864
Barot Pratham Janakkumar <barotpratham30@gmail.com>
2023-06-05 09:02:44 +0530

    mycart done

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -72,8 +72,8 @@ const Book = (props) => {
                 <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={quantity} min="1"/>
                 <button  onClick={()=>setQuantity(quantity+1)} class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </button>
               </div>
-        <button onClick={hadleAddToCart} className="ml-2  p-1.5 transition ease-in duration-200 rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">Add
-            to cart
+        <button onClick={hadleAddToCart} className="ml-2  p-1.5 transition ease-in duration-200 rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
+          Add to cart
             </button>
             </div>
         </>
